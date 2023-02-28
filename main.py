1be5f709c8f14d848ebe41565c1c66a7037e5079
Kofi Adu-Boateng <pastorkofi101@gmail.com>
2023-02-28 13:03:39 -0500

    Make graph.

--- main.py
+++ main.py
@@ -12,5 +12,9 @@ class Example1(Scene):
         t3 = MathTex(r"f : \mathbb{R}_+ \to \mathbb{R}, \ f(x) = \frac{1}{x}", color=RED_B).next_to(t2, RIGHT)
         self.play(Write(t3))
 
-        axes = Axes((-1, 6), (-1, 3))
-        self.play(Write(axes))
+        axes = Axes((-1, 6), (-1, 3)).shift(DOWN*1.5)
+        axes_labels = axes.get_axis_labels(x_label="x", y_label="y")
+        graph = axes.plot(lambda x: 1 / x, x_range=[0.333, 6], color=ORANGE)
+        graphing = VGroup(axes, graph, axes_labels)
+        self.play(DrawBorderThenFill(axes), Write(axes_labels))
+        self.play(Create(graph))
